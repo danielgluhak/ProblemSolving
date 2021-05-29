fdcdb063803b2a0848ebb9ffb3809eabc2b3d995
MojProfil <gluhakdaniel@gmail.com>
2021-05-29 23:26:24 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Array array = new Array();
-        Integer newArray[] = array.sortArrayDesc();
-        System.out.println(Arrays.toString(newArray));
+        MyArray array = new MyArray();
+        int[] myNerArray = array.getArray(3);
+
+        System.out.println("Min value: " + array.getMinValue(myNerArray));
     }
 }
